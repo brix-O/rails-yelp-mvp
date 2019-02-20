3d232b73600faa55226194af309275e16262df75
Ricardo Vieira <ricmvieira@gmail.com>
2019-02-20 14:26:22 +0000

    Challenge 2 Routes Completed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
-  CATEGORIES = %w(chinese italian japanese french belgian)
+  CATEGORIES = %w[chinese italian japanese french belgian]
 
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'Dishoom',
+    address: '7 Boundary St, London E2 7JE',
+    category: 'chinese'
+  },
+  {
+    name: 'Pizza East',
+    address: '56A Shoreditch High St, London E1 6PQ',
+    category: 'italian'
+  },
+  {
+    name: 'Trullo',
+    address: '300-302 St. Pauls Road | N1 2LH, London N1 2LH, England',
+    category: 'japanese'
+  },
+  {
+    name: "Leadbelly's Bar and Kitchen",
+    address: 'Deal Porter Square, London SE16 7AQ, England',
+    category: 'french'
+  },
+  {
+    name: 'Jose Pizarro',
+    address: '194 Bermondsey Street, London SE1 3TQ, England',
+    category: 'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
