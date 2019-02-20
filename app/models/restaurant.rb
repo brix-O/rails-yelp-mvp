833edd106f27372e750655ca2285539c3f62fdc2
Ricardo Vieira <ricmvieira@gmail.com>
2019-02-20 12:39:12 +0000

    Challenge 2: Models Created and Migrated

---
+++ app/models/restaurant.rb
@@ -0,0 +1,9 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  CATEGORIES = %w(chinese italian japanese french belgian)
+
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
+end
